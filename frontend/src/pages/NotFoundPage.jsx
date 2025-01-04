9ec4185b257a6b4fdf0f87852121e4461e6ff286
Abhishek Singh <singh.abhishek.16082000@gmail.com>
2025-01-04 15:32:57 +0530

    project completed

---
+++ frontend/src/pages/NotFoundPage.jsx
@@ -0,0 +1,9 @@
+const NotFoundPage = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <h1 className="text-red-600">404 Not found</h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
